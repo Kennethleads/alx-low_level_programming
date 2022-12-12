0bdb8ae87419a8a7624649a5beab10873d168f38
kenneth nwangbo <kennethnwangbo566@gmail.com>
2022-12-12 11:35:52 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print a C program using printf
+ *
+ * return: 0 if the program runs properly, non-zero otherwise
+ *
+ */
+
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);		
+}	

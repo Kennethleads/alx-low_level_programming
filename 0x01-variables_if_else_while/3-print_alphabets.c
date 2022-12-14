774cc4f4f15e663f8911f8b437fa1ca62c4375fd
kenneth nwangbo <kennethnwangbo566@gmail.com>
2022-12-13 23:31:50 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry ponit
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(d);
+			c++;
+		}
+	return (0);
+}
